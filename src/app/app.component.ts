347fa26d519bef177e4c8cdacf729f754ddf9e3d
daff <daffmg1@gmail.com>
2020-08-23 08:04:55 -0500

    ejemplo de tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private dataSer:DataService){}
   title = 'roberto-pruebas';
+  data:any=[];
+  columns:any=[];
+  ngOnInit(){
+    this.data=this.dataSer.getData();
+    this.columns=Object.keys(this.data[0]);
+    console.log(this.columns);
+    
+  }
 }
